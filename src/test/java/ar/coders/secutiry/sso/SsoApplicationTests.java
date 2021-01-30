f38a69cf97d3c16aab4773157de7879845ffee69
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-30 11:26:42 -0300

    initial commit

---
+++ src/test/java/ar/coders/secutiry/sso/SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.coders.secutiry.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
